3bc6f30e964532d004377a98359fedec339adffa
miaoyc <miaoyongchao@qianxin.com>
2021-09-13 18:48:36 +0800

    ====cicd:<1.0.3>: add pyproject.toml and setup.py====

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+File name    : setup.py
+Author       : miaoyc
+Create date  : 2021/9/13 6:41 下午
+Description  : 
+"""
+
+from setuptools import setup
+setup()
